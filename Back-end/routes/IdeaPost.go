14c53fd8f33fa9d039ad248b4d65f671d436c3a9
Teerasak Jumwanta <vgthailand@hotmail.com>
2022-09-21 01:32:11 +0700

    add ideapost-method getByID delete putUpdate

--- Back-end/routes/IdeaPost.go
+++ Back-end/routes/IdeaPost.go
@@ -9,4 +9,7 @@ import (
 func IdeaPostRoute(router *gin.Engine) {
 	router.GET("/", controller.GetIdeaPost)
 	router.POST("/", controller.PostIdeaPost)
+	router.GET("/:id", controller.GetbyIdIdeaPost)
+	router.DELETE("/:id", controller.DeleteIdeaPost)
+	router.PUT("/:id", controller.UpdateIdeaPost)
 }
