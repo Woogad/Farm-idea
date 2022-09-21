987e8bad2846455ca9a2559a63131e1174ac66b2
Teerasak Jumwanta <vgthailand@hotmail.com>
2022-09-21 09:06:05 +0700

    add interest

---
+++ front-end/src/components/Interest.js
@@ -0,0 +1,57 @@
+function Interest() {
+    return (
+        <div className='flex justify-center  '>
+            <div className="p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700  w-96 h-96 mt-36 ">
+                <div className='flex justify-center text-3xl'>
+                    <h1>ความสนใจ</h1>
+                </div>
+                <div className='grid grid-cols-4 gap-4 text-xs text-center content-center mt-8'>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        เครื่องมือ
+                    </button>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        เสริมสวย
+                    </button>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        วัสดุ
+                    </button>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        เครื่องดื่ม
+                    </button>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        ยาง
+                    </button>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        แปรรูป
+                    </button>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        อาหาร
+                    </button>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        ปุ๋ย
+                    </button>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        สุขภาพ
+                    </button>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded '>
+                        เครื่อง จักร
+                    </button>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        ตกแต่ง
+                    </button>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        ผ้า
+                    </button>
+
+                </div>
+                <div className='flex justify-center mt-8 text-2xl'>
+                    <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>
+                        เสร็จสิ้น
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Interest;
