323b22714d0a13ce2e5c3bc774d119e76c4954f7
Teerasak Jumwanta <vgthailand@hotmail.com>
2022-10-06 21:43:46 +0700

    add file idea-invest

---
+++ Back-end/routes/IdeaInvest.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github/Woogad/farm-idea/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func IdeaInvestRoute(router *gin.Engine) {
+	router.GET("/idea-invest", controller.GetIdeaInvest)
+	router.POST("/idea-invest", controller.PostIdeaInvest)
+}
